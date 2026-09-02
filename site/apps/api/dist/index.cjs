86342feeefa050552e0f87a2363ec1c30911504c
robosphere99 <anilalok99mahalpar@gmail.com>
2026-09-02 13:52:58 +0530

    fix(iis): add match url=.* and IsDirectory negate=true to universal IIS SPA rewrite rule

--- site/apps/api/dist/index.cjs
+++ site/apps/api/dist/index.cjs
@@ -14087,9 +14087,11 @@ function patchWebConfig() {
   <system.webServer>
     <rewrite>
       <rules>
-        <rule name="DynamicContent">
-          <conditions>
-            <add input="{REQUEST_FILENAME}" matchType="IsFile" negate="True" />
+        <rule name="DynamicContent" stopProcessing="true">
+          <match url=".*" />
+          <conditions logicalGrouping="MatchAll">
+            <add input="{REQUEST_FILENAME}" matchType="IsFile" negate="true" />
+            <add input="{REQUEST_FILENAME}" matchType="IsDirectory" negate="true" />
           </conditions>
           <action type="Rewrite" url="dist/index.cjs" />
         </rule>
